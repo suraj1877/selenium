cb67572e331212bab6d89a35fc0aa61c3f96edfc
suraj1607 <suraj1877patil@hotmail.com>
2024-05-18 13:09:51 +0530

    new branch push

--- src/test/java/testpkg/Welcome.java
+++ src/test/java/testpkg/Welcome.java
@@ -2,6 +2,7 @@ package testpkg;
 
 public class Welcome {
     public static void main(String[] args) {
-        System.out.println("Hello World!....");
+        System.out.println("Hello World of Selenium");
+        System.out.println("New branch created");
     }
 }
