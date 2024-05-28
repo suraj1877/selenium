09f01682cb30517110a737cd6cb626f8c55abf12
suraj1607 <suraj1877patil@hotmail.com>
2024-05-28 05:52:43 +0530

    remove unwanted pkg

--- src/test/java/com/w2a/logger/LoggerConfig.java
+++ src/test/java/com/w2a/logger/LoggerConfig.java
@@ -1,7 +1,6 @@
 package com.w2a.logger;
 
 import java.io.IOException;
-import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
